2aadda9b6d6797355eca5f09a0a2c16bf7542ff5
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-08-15 18:24:52 -0500

    /challenges/february url & view added and february formatted

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('This works!')
+def january(request):
+    return HttpResponse('Eat no meat for the entire month!')
+
+def february(request):
+    return HttpResponse('Walk for at least 20 minutes every day!')
