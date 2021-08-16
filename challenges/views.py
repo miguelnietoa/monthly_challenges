0b20c0740b1893df62180317c04b435aa1414153
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-08-16 13:41:18 -0500

    using render instead of render_to_string & HttpResponse

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 
 monthly_challenges = {
@@ -46,7 +45,6 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string('challenges/challenge.html')
-        return HttpResponse(response_data)
+        return render(request, 'challenges/challenge.html')
     except:
         return HttpResponseNotFound('<h1>This month is not supported!</h1>')
